c1aaee6c1c9e1caedaa25208aa392a9b8f4f857c
ColeT25 <116309785+ColeT25@users.noreply.github.com>
2023-02-16 14:04:03 -0500

    Making progress on stock scrapers and updating readme

--- stock_market_scrapers/stock_yahoo_scraper.py
+++ stock_market_scrapers/stock_yahoo_scraper.py
@@ -1,9 +1,9 @@
 import datetime
 import time
-import os
-import sys
 
 import pandas as pd
+import re
+import selenium.common.exceptions
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+# TODO THIS FILE IS VERY INCOMPLETE, SO KEEP THAT IN MIND
 def create_company_data_storage(*companies):
     data_struct = {}
     for company in companies:
@@ -43,24 +44,45 @@ def scrape_yahoo_for_stocks(*companies):
         driver.get('https://finance.yahoo.com/')
 
         time.sleep(4)
-        close_modal = driver.find_element(By.CLASS_NAME, 'close')
+        try:
+            close_modal = driver.find_element(By.CLASS_NAME, 'close')
+        except selenium.common.exceptions.NoSuchElementException:
+            close_modal = None
+
         if close_modal:
             close_modal.click()
-            time.sleep(1)
+            time.sleep(0.5)
 
         driver.find_element(By.NAME, 'yfin-usr-qry').send_keys(company)
-        time.sleep(5)
+        time.sleep(1)
 
         driver.find_element(By.ID, 'header-desktop-search-button').click()
         time.sleep(1)
 
-    # todo sign in modal exiting not currently working
-        sign_in_modal = driver.find_element(By.CLASS_NAME, 'Bxz(bb)')
-        if sign_in_modal:
-            sign_in_modal.find_element(By.CLASS_NAME, 'Pos(a)').click()
-            time.sleep(1)
+        try:
+            exit_login_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Maybe later')]")
+        except selenium.common.exceptions.NoSuchElementException:
+            exit_login_button = None
 
-    # todo fill up data structure and add code to screenshot full screen line for looking at
+        if exit_login_button:
+            exit_login_button.click()
+            time.sleep(0.5)
+
+        html_soup = BeautifulSoup(driver.page_source, features='html.parser')
+
+        # get market name
+        market_name = html_soup.find('h1', attrs={'class': "D(ib)"}).text
+        isolated_market_name = re.search(r'\((.*)\)', market_name).group(1)
+        scraped_data[company]['market name'] = isolated_market_name
+
+        # get current value
+        # todo found wrong value, need to adjust
+        scraped_data[company]['current value'] = html_soup.select(".e3b14781.f5a023e1")[0].text
+
+        # get market change percentage and market change value
+        #todo format this better, also not working yet
+        scraped_data[company]['market change percentage'] = float(html_soup.select(".e3b14781.f4be3290.f5a023e1")[0].text)
+        scraped_data[company]['market change value'] = float(html_soup.select(".e3b14781.f4be3290.e983cf79")[0].text)
 
     driver.close()
 
